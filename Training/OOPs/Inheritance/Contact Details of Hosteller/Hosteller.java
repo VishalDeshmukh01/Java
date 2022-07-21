e2240b7a64f179530be023867733decd1baf9829
VishalDeshmukh01 <109571605+VishalDeshmukh01@users.noreply.github.com>
2022-07-21 11:33:52 +0530

    Create Hosteller.java

---
+++ Training/OOPs/Inheritance/Contact Details of Hosteller/Hosteller.java
@@ -0,0 +1,20 @@
+public class Hosteller extends Student{
+ private String hostelName;
+ private int roomNo;
+ 
+    public String getHostelName() {
+        return hostelName;
+    }
+
+    public void setHostelName(String hostelName) {
+        this.hostelName = hostelName;
+    }
+
+    public int getRoomNo() {
+        return roomNo;
+    }
+
+    public void setRoomNo(int roomNo) {
+        this.roomNo = roomNo;
+    }
+}
